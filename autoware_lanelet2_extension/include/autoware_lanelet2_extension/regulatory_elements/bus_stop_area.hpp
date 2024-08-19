a96fb01d8d1b48aa79782e168a78b2f274f1cad1
Mamoru Sobue <mamoru.sobue@tier4.jp>
2024-08-19 21:39:59 +0900

    add bus_stop_area
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

---
+++ autoware_lanelet2_extension/include/autoware_lanelet2_extension/regulatory_elements/bus_stop_area.hpp
@@ -0,0 +1,77 @@
+// Copyright 2024 Tier IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef AUTOWARE_LANELET2_EXTENSION__REGULATORY_ELEMENTS__BUS_STOP_AREA_HPP_
+#define AUTOWARE_LANELET2_EXTENSION__REGULATORY_ELEMENTS__BUS_STOP_AREA_HPP_
+
+// NOLINTBEGIN(readability-identifier-naming)
+
+#include <autoware_lanelet2_extension/regulatory_elements/Forward.hpp>
+
+#include <lanelet2_core/primitives/Lanelet.h>
+
+#include <memory>
+
+namespace lanelet::autoware
+{
+
+// TODO(Mamoru Sobue): when updating existing elements from format_v1, prefix this with inline
+namespace format_v2
+{
+class BusStopArea : public lanelet::RegulatoryElement
+{
+public:
+  using Ptr = std::shared_ptr<BusStopArea>;
+  static constexpr char RuleName[] = "bus_stop_area";
+
+  static Ptr make(Id id, const AttributeMap & attributes, const Polygons3d & bus_stop_areas)
+  {
+    return Ptr{new BusStopArea(id, attributes, bus_stop_areas)};
+  }
+
+  /**
+   * @brief get the relevant bus stop area
+   * @return bus stop area
+   */
+  [[nodiscard]] ConstPolygons3d BusStopAreas() const;
+  [[nodiscard]] Polygons3d BusStopAreas();
+
+  /**
+   * @brief add a new bus stop are
+   * @param primitive bus stop area to add
+   */
+  void addBusStopArea(const Polygon3d & primitive);
+
+  /**
+   * @brief remove a bus stop area
+   * @param primitive the primitive
+   * @return true if the bus stop area existed and was removed
+   */
+  bool removeBusStopArea(const Polygon3d & primitive);
+
+private:
+  BusStopArea(Id id, const AttributeMap & attributes, const Polygons3d & bus_stop_area);
+
+  // the following lines are required so that lanelet2 can create this object
+  // when loading a map with this regulatory element
+  friend class RegisterRegulatoryElement<BusStopArea>;
+  explicit BusStopArea(const lanelet::RegulatoryElementDataPtr & data);
+};
+}  // namespace format_v2
+
+}  // namespace lanelet::autoware
+
+// NOLINTEND(readability-identifier-naming)
+
+#endif  // AUTOWARE_LANELET2_EXTENSION__REGULATORY_ELEMENTS__BUS_STOP_AREA_HPP_
