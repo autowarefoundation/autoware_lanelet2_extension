c148815f4483a9acd51bd25abcb26a7a5a99381d
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2024-09-24 23:55:30 +0900

    fix(Python bindings): add missing dependency to lanelet2 (#32)
    Signed-off-by: Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>

--- autoware_lanelet2_extension_python/autoware_lanelet2_extension_python/projection/__init__.py
+++ autoware_lanelet2_extension_python/autoware_lanelet2_extension_python/projection/__init__.py
@@ -1,3 +1,4 @@
+import lanelet2  # noqa: F401 # isort: skip
 import autoware_lanelet2_extension_python._autoware_lanelet2_extension_python_boost_python_projection as _projection_cpp
 
 MGRSProjector = _projection_cpp.MGRSProjector
