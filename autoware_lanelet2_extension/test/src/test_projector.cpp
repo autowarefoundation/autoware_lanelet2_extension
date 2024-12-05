2814e8c672bdfb8d35f5f4ddd95154dc0d0c3577
Zhe Shen <lucidshenzhe@gmail.com>
2024-12-06 06:25:15 +0900

    test with different elevations
    Signed-off-by: Zhe Shen <lucidshenzhe@gmail.com>

--- autoware_lanelet2_extension/test/src/test_projector.cpp
+++ autoware_lanelet2_extension/test/src/test_projector.cpp
@@ -32,52 +32,38 @@ public:
 TEST(TestSuite, ForwardMGRSProjection)  // NOLINT for gtest
 {
   lanelet::projection::MGRSProjector projector;
-  // lat/lon in Tokyo
-  lanelet::GPSPoint gps_point;
-  gps_point.lat = 35.652832;
-  gps_point.lon = 139.839478;
-  gps_point.ele = 12.3456789;
-  lanelet::BasicPoint3d mgrs_point = projector.forward(gps_point);
+  // different elevation values
+  std::vector<double> elevation_values = {0.0, -50.0, 1000.0};  // sea level, below sea level, very high
+  for (const double ele : elevation_values) {
+    lanelet::GPSPoint gps_point;
+    gps_point.lat = 35.652832;
+    gps_point.lon = 139.839478;
+    gps_point.ele = ele;
 
-  // projected z value should not change
-  ASSERT_DOUBLE_EQ(mgrs_point.z(), gps_point.ele)
-    << "Forward projected z value should be " << gps_point.ele;
+    lanelet::BasicPoint3d mgrs_point = projector.forward(gps_point);
 
-  // https://www.movable-type.co.uk/scripts/latlong-utm-mgrs.html
-  // round the projected value to mm since the above reference only gives value
-  // in mm precision
-  ASSERT_EQ(projector.getProjectedMGRSGrid(), "54SUE") << "Projected grid should be "
-                                                       << "54SUE";
-  double rounded_x_mm = round(mgrs_point.x() * 1000) / 1000.0;
-  ASSERT_DOUBLE_EQ(rounded_x_mm, 94946.081) << "Forward projected x value should be " << 94946.081;
-  double rounded_y_mm = round(mgrs_point.y() * 1000) / 1000.0;
-  ASSERT_DOUBLE_EQ(rounded_y_mm, 46063.748) << "Forward projected y value should be " << 46063.748;
+    ASSERT_DOUBLE_EQ(mgrs_point.z(), gps_point.ele)
+      << "Forward projected z value should be " << gps_point.ele << " for elevation " << ele;
+  }
 }
 
 TEST(TestSuite, ReverseMGRSProjection)  // NOLINT for gtest
 {
   lanelet::projection::MGRSProjector projector;
-  lanelet::BasicPoint3d mgrs_point;
-  mgrs_point.x() = 94946.0;
-  mgrs_point.y() = 46063.0;
-  mgrs_point.z() = 12.3456789;
+  std::vector<double> elevation_values = {0.0, -50.0, 1000.0};  // sea level, below sea level, very high
 
-  projector.setMGRSCode("54SUE");
-  lanelet::GPSPoint gps_point = projector.reverse(mgrs_point);
+  for (const double ele : elevation_values) {
+    lanelet::BasicPoint3d mgrs_point;
+    mgrs_point.x() = 94946.0;
+    mgrs_point.y() = 46063.0;
+    mgrs_point.z() = ele;
 
-  // projected z value should not change
-  ASSERT_DOUBLE_EQ(gps_point.ele, mgrs_point.z())
-    << "Reverse projected z value should be " << mgrs_point.z();
+    projector.setMGRSCode("54SUE");
+    lanelet::GPSPoint gps_point = projector.reverse(mgrs_point);
 
-  // https://www.movable-type.co.uk/scripts/latlong-utm-mgrs.html
-  // round the projected value since the above reference only gives value up to
-  // precision of 1e-8
-  double rounded_lat = round(gps_point.lat * 1e8) / 1e8;
-  ASSERT_DOUBLE_EQ(rounded_lat, 35.65282525)
-    << "Reverse projected latitude value should be " << 35.65282525;
-  double rounded_lon = round(gps_point.lon * 1e8) / 1e8;
-  ASSERT_DOUBLE_EQ(rounded_lon, 139.83947721)
-    << "Reverse projected longitude value should be " << 139.83947721;
+    ASSERT_DOUBLE_EQ(gps_point.ele, mgrs_point.z())
+      << "Reverse projected z value should be " << mgrs_point.z() << " for elevation " << ele;
+  }
 }
 
 TEST(TestSuite, ForwardTransverseMercatorProjection)  // NOLINT for gtest
