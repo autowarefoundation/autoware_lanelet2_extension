4c45cef2782c63a83242be796e933bf3f8469be2
Mamoru Sobue <mamoru.sobue@tier4.jp>
2025-11-07 18:04:37 +0900

    test(lanelet2_extension_python): add test & test for import
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

---
+++ autoware_lanelet2_extension_python/test/test_import_regulatory_elements.py
@@ -0,0 +1,31 @@
+import unittest
+
+g_autoware_lanelet2_extension_python_imported = False
+
+try:
+    import autoware_lanelet2_extension_python.regulatory_elements
+    g_autoware_lanelet2_extension_python_imported = True
+except Exception as e:
+    print(e)
+
+
+class ImportTest(unittest.TestCase):
+    """
+    This test is intended to check
+    'initialization of _autoware_lanelet2_extension_python_boost_python_*** \
+    raised unreported exception'
+    when we import this library without `import lanelet2`.
+
+    We are preventing this by doing
+    ```
+    import lanelet2  # noqa: F401 # isort: skip
+    ```
+    in our __init__.py
+    """
+
+    def test_imported(self) -> None:
+        self.assertTrue(g_autoware_lanelet2_extension_python_imported)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ autoware_lanelet2_extension_python/test/test_import_utility.py
@@ -0,0 +1,32 @@
+import unittest
+
+g_autoware_lanelet2_extension_python_utility_imported = False
+
+try:
+    import autoware_lanelet2_extension_python.utility.query as query
+    import autoware_lanelet2_extension_python.utility.utilities as utilities
+    g_autoware_lanelet2_extension_python_utility_imported = True
+except Exception as e:
+    print(e)
+
+
+class ImportTest(unittest.TestCase):
+    """
+    This test is intended to check
+    'initialization of _autoware_lanelet2_extension_python_boost_python_*** \
+    raised unreported exception'
+    when we import this library without `import lanelet2`.
+
+    We are preventing this by doing
+    ```
+    import lanelet2  # noqa: F401 # isort: skip
+    ```
+    in our __init__.py
+    """
+
+    def test_imported(self) -> None:
+        self.assertTrue(g_autoware_lanelet2_extension_python_utility_imported)
+
+
+if __name__ == '__main__':
+    unittest.main()
