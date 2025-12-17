914f833221803f0349111936773e9d9d741815b8
Yuxuan Liu <619684051@qq.com>
2025-12-17 15:10:48 +0900

    Rename import for boost python projection module

--- autoware_lanelet2_extension_python/autoware_lanelet2_extension_python/projection/__init__.py
+++ autoware_lanelet2_extension_python/autoware_lanelet2_extension_python/projection/__init__.py
@@ -1,5 +1,5 @@
 import lanelet2  # noqa: F401 # isort: skip
-import autoware_lanelet2_extension_python._lanelet2_extension_python_boost_python_projection as _projection_cpp
+import autoware_lanelet2_extension_python._autoware_lanelet2_extension_python_boost_python_projection as _projection_cpp
 
 MGRSProjector = _projection_cpp.MGRSProjector
 MGRSProjector.__doc__ = """
