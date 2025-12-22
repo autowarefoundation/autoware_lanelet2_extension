e327e0cb2b08d425c86e295cffa832872afdc3b0
Yuxuan Liu <619684051@qq.com>
2025-12-22 13:16:12 +0900

    feat: add tmp to python API (#90)
    * feat: add TMProjector in python API
    
    Also add code docs for easier usage in IDE.
    
    * Rename import for boost python projection module

--- autoware_lanelet2_extension_python/autoware_lanelet2_extension_python/projection/__init__.py
+++ autoware_lanelet2_extension_python/autoware_lanelet2_extension_python/projection/__init__.py
@@ -2,3 +2,16 @@ import lanelet2  # noqa: F401 # isort: skip
 import autoware_lanelet2_extension_python._autoware_lanelet2_extension_python_boost_python_projection as _projection_cpp
 
 MGRSProjector = _projection_cpp.MGRSProjector
+MGRSProjector.__doc__ = """
+    MGRSProjector(origin: lanelet2.io.Origin)
+    Example:
+        origin = lanelet2.io.Origin(lat, lon, alt)
+        projector = MGRSProjector(origin)
+    """
+TransverseMercatorProjector = _projection_cpp.TransverseMercatorProjector
+TransverseMercatorProjector.__doc__ = """
+    TransverseMercatorProjector(origin: lanelet2.io.Origin)
+    Example:
+        origin = lanelet2.io.Origin(lat, lon, alt)
+        projector = TransverseMercatorProjector(origin)
+    """
